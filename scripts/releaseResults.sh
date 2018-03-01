d672b071d7a9fd4735b89f2807117016a60a2586
Withana <swithana@156-56-176-223.dhcp-bl.indiana.edu>
2018-03-01 13:58:02 -0500

    fixing a resuts release permissions for the serial device

--- scripts/releaseResults.sh
+++ scripts/releaseResults.sh
@@ -3,3 +3,4 @@
 cp $USER_HOME_DIR/uploads/releaseresults /usr/local/bin/releaseresults
 chown $SSH_USERNAME /usr/local/bin/releaseresults
 chmod 744 /usr/local/bin/releaseresults
+chmod 666 /dev/ttyS0
