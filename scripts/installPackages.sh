063a20ca9ff5f2c984ef849f796c38a71d75b09c
Sachith Danushka Withana <swithana@crossbill.sice.indiana.edu>
2018-05-31 12:00:45 -0400

    adding and upgrading pip and packages

--- scripts/installPackages.sh
+++ scripts/installPackages.sh
@@ -3,13 +3,15 @@ apt-get update
 apt-get -y upgrade
 
 #generic ubuntu packages
-apt-get install -y git maven python-pip parallel curl htop iotop jq pcregrep zsh python3-pip
+apt-get install -y git maven parallel curl htop iotop jq pcregrep zsh python-setuptools python-dev python3-setuptools
 
 #python 2.7 packages
-pip install numpy scipy matplotlib pandas nltk regex GenSim ujson dask toolz theano csvkit htrc-feature-reader
+easy_install pip
+pip install numpy scipy matplotlib pandas nltk regex GenSim ujson dask toolz theano csvkit htrc-feature-reader wget cython topicexplorer htrc
 
 #python 3 packages
-pip3 install numpy scipy matplotlib pandas nltk regex GenSim ujson dask toolz theano csvkit htrc-feature-reader
+easy_install3 pip
+pip3 install numpy scipy matplotlib pandas nltk regex GenSim ujson dask toolz theano csvkit htrc-feature-reader wget cython topicexplorer htrc
 
 #fixing the terminal LANG issue
 locale-gen
